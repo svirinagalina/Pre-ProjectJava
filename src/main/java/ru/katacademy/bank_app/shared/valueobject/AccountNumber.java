3496e7178f69d1921a5c7725b5b6a870edb9c170
Rizvan-Guno <rizvangunaev95@gmail.com>
2025-04-15 17:56:26 +0300

    feature/[#10] Реализовал AccountNumber value object

---
+++ src/main/java/ru/katacademy/bank_app/shared/valueobject/AccountNumber.java
@@ -0,0 +1,37 @@
+package ru.katacademy.bank_app.shared.valueobject;
+
+/**
+ * Представляет объект номера счета.
+ * <p>
+ * Этот класс гарантирует, что номер счета состоит ровно из 20 цифр.
+ * При создании объекта проверяется, что строка:
+ * <ul>
+ *   <li>не равна null,</li>
+ *   <li>Содержит ровно 20 символов,</li>
+ *   <li>Состоит только из цифр.</li>
+ * </ul>
+ * Если эти условия не выполнены, выбрасывается {@link IllegalArgumentException}.
+ * </p>
+ */
+public record AccountNumber(String accountNumber) {
+
+    /**
+     * Проверяет, что номер счета состоит ровно из 20 цифр.
+     * Если это не так, выбрасывается исключение {@link IllegalArgumentException}.
+     *
+     * @param accountNumber Номер счета, который должен быть строкой длиной ровно 20 символов, содержащей только цифры.
+     * @throws IllegalArgumentException Если номер счета некорректен (не 20 символов или содержит нецифровые символы).
+     */
+    public AccountNumber {
+        if (accountNumber == null || accountNumber.length() != 20 || !accountNumber.matches("\\d+")) {
+            throw new IllegalArgumentException("Account number must be exactly 20 digits long and contain only digits.");
+        }
+    }
+
+    /**
+     * @return строковое представление номера счёта
+     */
+    public String value() {
+        return accountNumber;
+    }
+}
