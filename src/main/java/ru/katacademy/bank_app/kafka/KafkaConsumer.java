932139fa2a4ec845c567d6a09598920352ccb54b
Rizvan-Guno <rizvangunaev95@gmail.com>
2025-04-22 19:45:31 +0300

    feature/[#35] Добавил документацию

--- src/main/java/ru/katacademy/bank_app/kafka/KafkaConsumer.java
+++ src/main/java/ru/katacademy/bank_app/kafka/KafkaConsumer.java
@@ -4,10 +4,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+/**
+ * Сервис, который подписывается на топики в Kafka и обрабатывает полученные сообщения.
+ */
 @Slf4j
 @Service
 public class KafkaConsumer {
-
+    /**
+     * Метод для получения сообщений из Kafka-топика и их логирования.
+     * Этот метод автоматически вызывается, когда поступает новое сообщение в указанный топик.
+     *
+     * @param message Сообщение, полученное из Kafka топика.
+     */
     @KafkaListener(topics = "course", groupId = "my_consumer")
     public void listen(String message) {
         log.info("Received message = " + message);
--- src/main/java/ru/katacademy/bank_app/kafka/KafkaProducer.java
+++ src/main/java/ru/katacademy/bank_app/kafka/KafkaProducer.java
@@ -4,16 +4,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+/**
+ * Класс для отправки сообщений в топики Kafka.
+ * Использует {@link KafkaTemplate} для асинхронной отправки сообщений в указанный топик.
+ */
 @Service
 public class KafkaProducer {
-
     private final KafkaTemplate<String, String> kafkaTemplate;
 
+    /**
+     * Конструктор для внедрения зависимости KafkaTemplate.
+     *
+     * @param kafkaTemplate {@link KafkaTemplate} для работы с Kafka.
+     */
     @Autowired
     public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
+    /**
+     * Отправляет сообщение в топик Kafka асинхронно.
+     *
+     * @param message сообщение, которое будет отправлено в топик "course".
+     */
     public void send(String message) {
         kafkaTemplate.send("course", message);
     }
