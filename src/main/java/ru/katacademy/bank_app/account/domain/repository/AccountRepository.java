51f4a1b65f512719191fe2cc509c49c7670bc290
Sheffy <ishevchenko.id2000@yandex.ru>
2025-04-24 16:06:18 +0300

    [#31] .

--- src/main/java/ru/katacademy/bank_app/account/domain/repository/AccountRepository.java
+++ src/main/java/ru/katacademy/bank_app/account/domain/repository/AccountRepository.java
@@ -2,6 +2,7 @@ package ru.katacademy.bank_app.account.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.katacademy.bank_app.account.domain.entity.Account;
+import ru.katacademy.bank_app.account.infrastructure.persistence.entity.AccountEntity;
 import ru.katacademy.bank_app.shared.valueobject.AccountNumber;
 
 import java.util.Optional;
@@ -9,7 +10,7 @@ import java.util.Optional;
 /**
  * Репозиторий для работы с аккаунтами
  */
-public interface AccountRepository extends JpaRepository<Account, Long> {
+public interface AccountRepository extends JpaRepository<AccountEntity, Long> {
 
     /**
      * Находит аккаунт по номеру счета.
@@ -17,5 +18,5 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
      * @param accountNumber номер счета аккаунта
      * @return Optional с найденным аккаунтом, если он есть
      */
-    Optional<Account> findByAccountNumber(AccountNumber accountNumber);
+    Optional<AccountEntity> findByAccountNumber(AccountNumber accountNumber);
 }
