0dfb31d5c28b090933828bc1aa3dcce7adaeb2b8
Rizvan-Guno <rizvangunaev95@gmail.com>
2025-04-29 22:32:49 +0300

    feature/[#48] Вынес логику валидации переводов в сервис TransferValidator и добавил BusinessErrorCode

--- src/main/java/ru/katacademy/bank_app/account/application/service/AccountService.java
+++ src/main/java/ru/katacademy/bank_app/account/application/service/AccountService.java
@@ -36,6 +36,7 @@ public class AccountService {
     private final AccountRepository accountRepository;
     private final NotificationService notificationService;
     private final TransferEventPublisher eventPublisher;
+    private final TransferValidator validator;
 
     /**
      * Переводит денежные средства от одного аккаунта к другому.
@@ -49,7 +50,7 @@ public class AccountService {
      * @param from   аккаунт отправителя
      * @param to     аккаунт получателя
      * @param amount сумма перевода (объект {@link Money})
-     * @throws IllegalArgumentException        если {@code from} или {@code to} равны {@code null}
+     * @throws IllegalArgumentException       если {@code from} или {@code to} равны {@code null}
      * @throws BusinessRuleViolationException если аккаунт отправителя-получателя не активен,
      *                                        валюты не совпадают или сумма депозита меньше или сумма списания больше или ровна нулю
      * @throws CurrencyMismatchException      если валюты не совпадают
@@ -67,7 +68,7 @@ public class AccountService {
         final Account accountFrom = getAccountByAccountNumber(from);
         final Account accountTo = getAccountByAccountNumber(to);
 
-        accountFrom.validateTransferTo(accountTo, amount);
+        validator.validateTransferTo(accountFrom, accountTo, amount);
         accountFrom.withdraw(amount);
         accountTo.deposit(amount);
 
