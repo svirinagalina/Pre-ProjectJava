c197623c188ac06f7519d335e7b19bed05acf3e5
Rizvan-Guno <rizvangunaev95@gmail.com>
2025-05-12 19:02:35 +0300

    feature/#57: добавил то что не подтянул при первом коммите

---
+++ user-service/src/test/java/ru/katacademy/bank_app/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package ru.katacademy.bank_app.user_service;
+
+import org.junit.jupiter.api.Test;
+
+//@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
