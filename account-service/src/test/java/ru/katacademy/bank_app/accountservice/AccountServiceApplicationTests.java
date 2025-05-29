2ae8f621b27a708a083df2e4c4d10cc947df7ed8
Perpulco <pupshev16@gmail.com>
2025-05-29 16:40:50 +0500

    Настройка нового сервиса

--- account-service/src/test/java/ru/katacademy/bank_app/accountservice/AccountServiceApplicationTests.java
+++ account-service/src/test/java/ru/katacademy/bank_app/accountservice/AccountServiceApplicationTests.java
@@ -3,7 +3,7 @@ package ru.katacademy.bank_app.accountservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class AccountServiceApplicationTests {
 
     @Test
