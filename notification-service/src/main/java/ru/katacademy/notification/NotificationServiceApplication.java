e2b1f7d5d4dfaad7fd7ddc03dcedf61040891481
AlexeyZat <a.out@internet.ru>
2025-06-23 17:09:13 +0300

    Исправил ошибки: Настроена конфигурация соединение с бд. Контейнеры поднимаются Ошибок нет

--- notification-service/src/main/java/ru/katacademy/notification/NotificationServiceApplication.java
+++ notification-service/src/main/java/ru/katacademy/notification/NotificationServiceApplication.java
@@ -1,12 +1,23 @@
 package ru.katacademy.notification;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
+@EnableJpaRepositories("ru.katacademy.notification.infrastructure.persistence.repository")
+@EntityScan("ru.katacademy.notification.infrastructure.persistence.entity")
 public class NotificationServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner logProfile(@Value("${spring.profiles.active:}") String profile) {
+        return args -> System.out.println(">>> Active Spring profile: " + profile);
     }
 }
