aaa2cc11222ff9a47b79114f56a1f456b673cfa8
KakoyToFenix <kirusina57@mail.ru>
2025-08-02 00:42:16 +0300

    Убрал лишнюю настройку свойств KafkaConsumerConfig для работоспосбности тестов, которая есть в application

--- notification-service/src/main/java/ru/katacademy/notification/infrastructure/config/KafkaConsumerConfig.java
+++ notification-service/src/main/java/ru/katacademy/notification/infrastructure/config/KafkaConsumerConfig.java
@@ -1,46 +1,29 @@
 package ru.katacademy.notification.infrastructure.config;
 
-import jakarta.annotation.PostConstruct;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.TopicPartition;
-import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
 import org.springframework.kafka.listener.DefaultErrorHandler;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.util.backoff.FixedBackOff;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Configuration
+@EnableKafka
 public class KafkaConsumerConfig {
 
     @Bean
-    public ConsumerFactory<String, Object> consumerFactory() {
-        JsonDeserializer<Object> deserializer = new JsonDeserializer<>();
-        deserializer.addTrustedPackages("ru.katacademy.*");
-        deserializer.setUseTypeHeaders(false);
+    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(
+            ConsumerFactory<String, Object> consumerFactory,
+            DefaultErrorHandler errorHandler) {
 
-        Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "notification-service");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
-
-        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), deserializer);
-    }
-
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, Object> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory());
+        factory.setConsumerFactory(consumerFactory);
+        factory.setCommonErrorHandler(errorHandler);
         return factory;
     }
 
@@ -55,9 +38,5 @@ public class KafkaConsumerConfig {
                 kafkaTemplate,
                 (record, ex) -> new TopicPartition(record.topic() + ".error", record.partition())
         );
-    }
-    @PostConstruct
-    public void printKafkaProps() {
-        System.out.println(">>> KafkaConsumerConfig активирован вручную (BOOTSTRAP kafka:9092)");
     }
 }
