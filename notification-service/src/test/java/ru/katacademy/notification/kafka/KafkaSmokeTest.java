fb2a849c5121cc0ae457bf84013a2806228f591d
KakoyToFenix <kirusina57@mail.ru>
2025-08-01 22:55:56 +0300

    Переименовал application-test, убрал комменатрии в тесте

--- notification-service/src/test/java/ru/katacademy/notification/kafka/KafkaSmokeTest.java
+++ notification-service/src/test/java/ru/katacademy/notification/kafka/KafkaSmokeTest.java
@@ -4,17 +4,23 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-//@SpringBootTest
-//public class KafkaSmokeTest {
-//
-//    @Autowired
-//    KafkaTemplate<String, Object> kafkaTemplate;
-//
-//    @Test
-//    void contextLoads_andKafkaTemplateWorks() {
-//        assertThat(kafkaTemplate).isNotNull();
-//    }
-//}
+@SpringBootTest
+@ActiveProfiles("test")
+public class KafkaSmokeTest {
+
+    @Autowired
+    KafkaTemplate<String, Object> kafkaTemplate;
+
+    @Test
+    void contextLoads_andKafkaTemplateWorks() {
+        assertThat(kafkaTemplate).isNotNull();
+    }
+
+
+}
