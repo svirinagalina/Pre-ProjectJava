98a7cd1b97b9b2881147b0d4ebdbb614a0935e0b
Евгений Урбагаев <evgenijurbagaev@EvgenijdeMacBook-Air.local>
2025-09-25 22:35:10 +0300

    Реализована обработка ошибок при разборе JWT

--- security-service/src/main/java/ru/katacademy/securityservice/SecurityServiceApplication.java
+++ security-service/src/main/java/ru/katacademy/securityservice/SecurityServiceApplication.java
@@ -3,7 +3,7 @@ package ru.katacademy.securityservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "ru.katacademy.securityservice")
 public class SecurityServiceApplication {
 
 	public static void main(String[] args) {
