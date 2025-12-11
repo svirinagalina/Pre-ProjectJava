8761c6f195fd1f623bbf3219eff3445817be374d
Kirill <Rock-LioN@mail.ru>
2025-12-11 21:29:04 +0300

    Add AccountServiceExceptionHandler and update ErrorResponse

---
+++ account-service/src/main/java/ru/katacademy/bank_app/accountservice/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package ru.katacademy.bank_app.accountservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//Временное решение для разрешения на doc
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
