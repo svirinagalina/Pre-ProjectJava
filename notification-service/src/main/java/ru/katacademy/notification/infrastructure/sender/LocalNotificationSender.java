89ddcbb0de84c67ef8b2a21b477b4cde9349d295
Dmitrii Krasitskii <maleus997@mail.ru>
2025-12-19 17:07:43 +0300

    добавлены описание для классов заглушек, так же переопределены порты для локального запуска сервисов

--- notification-service/src/main/java/ru/katacademy/notification/infrastructure/sender/LocalNotificationSender.java
+++ notification-service/src/main/java/ru/katacademy/notification/infrastructure/sender/LocalNotificationSender.java
@@ -6,6 +6,21 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import ru.katacademy.notification.application.sender.NotificationSender;
 
+/**
+ * Локальная заглушка для публикации событий смены KYC-статуса
+ * <p>
+ * Этот компонент используется в профиле {@code local} и не взаимодействует с Kafka.
+ * Вместо отправки сообщений в топик, событие просто логируется.
+ * Позволяет запускать сервис локально, без подключения к внешней инфраструктуре.
+ * <p>
+ * Использование:
+ * - Поддерживает интерфейс {@link NotificationSender}, чтобы NotificationServiceImpl мог вызывать метод send
+ * - Сохраняется порядок вызовов и семантика публикации, но события не уходят в реальную систему
+ * - Позволяет тестировать бизнес-логику NotificationServiceImpl без ошибок зависимостей
+ * <p>
+ * Дата: 19-12-2025
+ * Автор: Dmitrii Krasitskii
+ */
 @Component
 @Profile("local")
 public class LocalNotificationSender implements NotificationSender {
--- user-settings-service/src/main/java/ru/katacademy/bank_app/settingsservice/adapter/out/kafka/LocalSettingsChangedKafkaPublisher.java
+++ user-settings-service/src/main/java/ru/katacademy/bank_app/settingsservice/adapter/out/kafka/LocalSettingsChangedKafkaPublisher.java
@@ -7,6 +7,21 @@ import org.springframework.stereotype.Component;
 import ru.katacademy.bank_app.settingsservice.application.port.out.SettingsChangedEventPublisher;
 import ru.katacademy.bank_shared.event.SettingsChangedEvent;
 
+/**
+ * Локальная заглушка для публикации событий смены KYC-статуса
+ * <p>
+ * Этот компонент используется в профиле {@code local} и не взаимодействует с Kafka.
+ * Вместо отправки сообщений в топик, событие просто логируется.
+ * Позволяет запускать сервис локально, без подключения к внешней инфраструктуре.
+ * <p>
+ * Использование:
+ * - Поддерживает интерфейс {@link SettingsChangedEventPublisher}, чтобы UserRegisterServiceImpl мог вызывать метод publish
+ * - Сохраняется порядок вызовов и семантика публикации, но события не уходят в реальную систему
+ * - Позволяет тестировать бизнес-логику UserRegisterServiceImpl без ошибок зависимостей
+ * <p>
+ * Дата: 19-12-2025
+ * Автор: Dmitrii Krasitskii
+ */
 @Component
 @Profile("local")
 public class LocalSettingsChangedKafkaPublisher implements SettingsChangedEventPublisher {
