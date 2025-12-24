91f427f24bbe5c2bf760980508d6cc5ea6824a1e
Dmitrii Krasitskii <maleus997@mail.ru>
2025-12-24 15:23:15 +0300

    cors для профиля local отключен, все сервисы корректно отдают JSON при переходе на v3/api-docs, так же все сервисы корректно отображают эндпоинты при переходе на swagger-ui/index.html#/

---
+++ fraud-detection/src/main/java/ru/katacademy/bank_app/frauddetection/config/CorsLocalConfig.java
@@ -0,0 +1,23 @@
+package ru.katacademy.bank_app.frauddetection.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * отключение CORS для корректной работы Swagger при поднятии приложения с профилем local
+ */
+@Profile("local")
+@Configuration
+public class CorsLocalConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
