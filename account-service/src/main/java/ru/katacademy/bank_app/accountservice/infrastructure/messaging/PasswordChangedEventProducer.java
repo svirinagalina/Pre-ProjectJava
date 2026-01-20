0f11b93e8c94622ab488e07e9195d1b7650a7228
Daniil1994-hub <tarandaniil22@gmail.com>
2026-01-20 20:24:49 +0300

    Перевел модуль bank-shared к роли библиотеки.

---
+++ account-service/src/main/java/ru/katacademy/bank_app/accountservice/infrastructure/messaging/PasswordChangedEventProducer.java
@@ -0,0 +1,23 @@
+package ru.katacademy.bank_app.accountservice.infrastructure.messaging;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+import ru.katacademy.bank_shared.event.notification.PasswordChangedEvent;
+
+@Component
+@RequiredArgsConstructor
+public class PasswordChangedEventProducer {
+
+    private final KafkaTemplate<String, PasswordChangedEvent> kafkaTemplate;
+
+    public void sendPasswordChangedEvent(Long userId) {
+        final PasswordChangedEvent event = new PasswordChangedEvent();
+        event.setUserId(userId.toString());
+        event.setEventType("PASSWORD_CHANGED");
+        event.setOccurredAt(System.currentTimeMillis());
+        event.setSource("account-service");
+
+        kafkaTemplate.send("password-changed-topic", event);
+    }
+}
